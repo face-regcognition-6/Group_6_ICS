b5091fa5679de7d4be51f4ad85cd6c12f2fc5f3a
MeTHANE <140156732+VincentMeTHANE@users.noreply.github.com>
2024-09-27 17:54:27 +0800

    Delete djangoProject directory

--- djangoProject/routing.py
+++
@@ -1,7 +0,0 @@
-# chat/routing.py
-from django.urls import re_path
-import video.consumer
-
-websocket_urlpatterns = [
-	re_path(r'video/(?P<v_name>\w+)/$', video.consumer.VideoConsumer.as_asgi())
-]
